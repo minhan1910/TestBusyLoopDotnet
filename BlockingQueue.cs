ac7a89afced42e4ff569de378a7735d1a297eca4
minhan1910 <annogo123@gmail.com>
2025-04-23 16:46:28 +0700

    init

---
+++ BlockingQueue.cs
@@ -0,0 +1,24 @@
+class BlockingQueue
+{
+    private readonly List<string> _queue = [];
+
+    public void Enqueue(string item)
+    {
+        _queue.Add(item);            
+    }
+
+    public bool IsEmpty()
+    {
+        return _queue.Count == 0;
+    }
+
+    public string Dequeue()
+    {
+        while (IsEmpty()) {}
+
+        var item = _queue[0];
+        _queue.RemoveAt(0);
+
+        return item;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿var queue = new BlockingQueue();
+var t1 = new Thread(DeQueueThread) { IsBackground = true };
+t1.Start();
+
+string? s = null;
+
+do
+{
+    Console.Write("S: ");
+    s = Console.ReadLine();
+    if (!string.IsNullOrEmpty(s))
+    {
+        queue.Enqueue(s);
+    }
+    Thread.Sleep(100);
+} while (!string.IsNullOrEmpty(s));
+
+void DeQueueThread()
+{
+    while (true)
+    {
+        while (queue.IsEmpty() == false)
+        {
+            var item = queue.Dequeue();
+            Console.WriteLine($"Q: {item}");
+        }
+    }
+}
